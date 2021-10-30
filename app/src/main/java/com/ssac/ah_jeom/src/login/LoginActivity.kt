ecf1c8f4b8c08a7a6320c9964a12080f292b580f
Won Seok Kim <76798309+dnjstjr245@users.noreply.github.com>
2021-10-31 05:39:57 +0900

    Add(layout): splash & login

---
+++ app/src/main/java/com/ssac/ah_jeom/src/login/LoginActivity.kt
@@ -0,0 +1,13 @@
+package com.ssac.ah_jeom.src.login
+
+import android.os.Bundle
+import com.ssac.ah_jeom.config.BaseActivity
+import com.ssac.ah_jeom.databinding.ActivityLoginBinding
+
+class LoginActivity : BaseActivity<ActivityLoginBinding>(ActivityLoginBinding::inflate) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+    }
+}
--- app/src/main/java/com/ssac/ah_jeom/src/splash/SplashActivity.kt
+++ app/src/main/java/com/ssac/ah_jeom/src/splash/SplashActivity.kt
@@ -4,16 +4,16 @@ import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
-import com.ssac.ah_jeom.src.main.MainActivity
 import com.ssac.ah_jeom.config.BaseActivity
 import com.ssac.ah_jeom.databinding.ActivitySplashBinding
+import com.ssac.ah_jeom.src.login.LoginActivity
 
 class SplashActivity : BaseActivity<ActivitySplashBinding>(ActivitySplashBinding::inflate) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         Handler(Looper.getMainLooper()).postDelayed({
-            startActivity(Intent(this, MainActivity::class.java))
+            startActivity(Intent(this, LoginActivity::class.java))
             finish()
         }, 1500)
     }
