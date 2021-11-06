8bb4d6948beb4cb3ca21041a19d99baecec72869
Won Seok Kim <76798309+dnjstjr245@users.noreply.github.com>
2021-11-07 01:34:28 +0900

    Feat(layout): infinite sliding banner images

--- app/src/main/java/com/ssac/ah_jeom/src/main/home/HomeFragment.kt
+++ app/src/main/java/com/ssac/ah_jeom/src/main/home/HomeFragment.kt
@@ -1,9 +1,73 @@
 package com.ssac.ah_jeom.src.main.home
 
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.os.Message
+import android.view.View
+import androidx.viewpager2.widget.ViewPager2
 import com.ssac.ah_jeom.R
 import com.ssac.ah_jeom.config.BaseFragment
 import com.ssac.ah_jeom.databinding.FragmentHomeBinding
+import com.ssac.ah_jeom.src.main.home.adapter.HomeFragmentViewpagerAdapter
 
 class HomeFragment : BaseFragment<FragmentHomeBinding>(FragmentHomeBinding::bind, R.layout.fragment_home){
 
+    private var numBanner = 3
+    private var currentPosition = 2
+    private var myHandler = MyHandler()
+    private val intervalTime = 5000.toLong() // 몇초 간격으로 페이지를 넘길것인지 (5000 = 5.0초)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.fragmentHomeBannerViewpager.adapter = HomeFragmentViewpagerAdapter(getBannerList())
+        binding.fragmentHomeBannerViewpager.orientation = ViewPager2.ORIENTATION_HORIZONTAL
+        binding.fragmentHomeBannerViewpager.setCurrentItem(currentPosition, false) // 현재 위치를 지정
+
+        // 현재 몇 번째 배너인지 보여주는 숫자를 변경함
+        val apply = binding.fragmentHomeBannerViewpager.apply {
+            registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+
+                override fun onPageScrollStateChanged(state: Int) {
+                    super.onPageScrollStateChanged(state)
+                    when (state) {
+                        // 뷰페이저에서 손 떼었을때 / 뷰페이저 멈춰있을 때
+                        ViewPager2.SCROLL_STATE_IDLE -> autoScrollStart(intervalTime)
+
+                        // 뷰페이저 움직이는 중
+                        ViewPager2.SCROLL_STATE_DRAGGING -> autoScrollStop()
+                    }
+                }
+
+            })
+        }
+
+        binding.fragmentHomeBannerViewpager.setCurrentItem(++currentPosition, true)
+
+    }
+
+    private fun getBannerList(): ArrayList<Int> {
+        return arrayListOf(R.drawable.banner_1_temp, R.drawable.banner_2_temp, R.drawable.banner_3_temp)
+    }
+
+    private fun autoScrollStart(intervalTime: Long) {
+        myHandler.removeMessages(0) // 이거 안하면 핸들러가 1개, 2개, 3개 ... n개 만큼 계속 늘어남
+        myHandler.sendEmptyMessageDelayed(0, intervalTime) // intervalTime 만큼 반복해서 핸들러를 실행하게 함
+    }
+
+    private fun autoScrollStop() {
+        myHandler.removeMessages(0) // 핸들러를 중지시킴
+    }
+
+    private inner class MyHandler : Handler(Looper.getMainLooper()) {
+        override fun handleMessage(msg: Message) {
+            super.handleMessage(msg)
+
+            if (msg.what == 0) {
+                binding.fragmentHomeBannerViewpager.setCurrentItem(++currentPosition, true) // 다음 페이지로 이동
+                autoScrollStart(intervalTime) // 스크롤을 계속 이어서 한다.
+            }
+        }
+    }
 }
---
+++ app/src/main/java/com/ssac/ah_jeom/src/main/home/adapter/HomeFragmentViewpagerAdapter.kt
@@ -0,0 +1,32 @@
+package com.ssac.ah_jeom.src.main.home.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.recyclerview.widget.RecyclerView
+import com.ssac.ah_jeom.R
+
+class HomeFragmentViewpagerAdapter(bannerList: ArrayList<Int>) : RecyclerView.Adapter<HomeFragmentViewpagerAdapter.PagerViewHolder>() {
+
+    var item = bannerList
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int) = PagerViewHolder((parent))
+
+    override fun getItemCount(): Int {
+        return Int.MAX_VALUE
+    } // 아이템의 갯수를 임의로 확 늘린다. 마치 무한으로 스크롤 되는 것처럼
+
+    override fun onBindViewHolder(holder: PagerViewHolder, position: Int) {
+        holder.banner.setImageResource(item[position%3])
+    }
+
+    inner class PagerViewHolder(parent: ViewGroup) : RecyclerView.ViewHolder(
+        LayoutInflater.from(parent.context).inflate(
+            R.layout.fragment_home_banner_item, parent, false)) {
+        val banner = itemView.findViewById<ImageView>(R.id.fragment_home_banner_item_image)
+    }
+
+
+}
