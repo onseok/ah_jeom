d41fc49898892789128169db88d94536eaa0f21c
Kim Won Seok <dnjstjr245@gmail.com>
2021-11-22 17:13:44 +0900

    Feat(layout): SoaringArtistActivity

---
+++ app/src/main/java/com/ssac/ah_jeom/src/main/subscribe/soaringArtist/SoaringArtistActivity.kt
@@ -0,0 +1,11 @@
+package com.ssac.ah_jeom.src.main.subscribe.soaringArtist
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SoaringArtistActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_soaring_artist)
+    }
+}
